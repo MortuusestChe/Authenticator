50608b7114e559efaf8ba5147dd3a1255b4b44b1
Enis Çakırçalı <44526564+MortuusestChe@users.noreply.github.com>
2021-12-13 19:59:54 +0300

    Create dbconfig.js

---
+++ config/dbconfig.js
@@ -0,0 +1,4 @@
+module.exports = {
+    secret : 'yoursecret',
+    database : 'mongodb+srv://yournicknameandpassword@yourclusternamecr2yp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
