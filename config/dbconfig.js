3d6b12c92316aa9d5c25bb0e24bf06ab7766db6a
Enis Çakırçalı <44526564+MortuusestChe@users.noreply.github.com>
2021-12-13 19:54:45 +0300

    Update dbconfig.js

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -1,4 +1,4 @@
 module.exports = {
     secret : 'yoursecret',
-    database : 'mongodb+srv://KarlWodanaz:MortuusestChe41@cluster0.cr2yp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    database : 'mongodb+srv://yournicknameandpassword@cluster0.cr2yp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
